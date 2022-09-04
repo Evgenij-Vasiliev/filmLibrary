0e4535cfd23b9971f594962f546d2ce78caa1bfa
Evgenij-Vasiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2022-09-04 23:02:40 +0300

    first level the end

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-const lastMovieTitle = prompt("Какой фильм вы посмотрели последним?");
-const rating = prompt("Во сколько баллов(до 10) оцениваете фильм?");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const lastMovieTitle = prompt('Один из последних просмотренных фильмов?', '');
+const rating = +prompt('Во сколько баллов(до 10) оцениваете фильм?', '');
+
+const lastMovieTitle2= prompt('Один из последних просмотренных фильмов??', '');
+const rating2= +prompt('Во сколько баллов(до 10) оцениваете фильм?', '');
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
+    actors: {},
     genres: [],
     privat: false,
 };
 
 personalMovieDB.movies[lastMovieTitle] = rating;
+personalMovieDB.movies[lastMovieTitle2] = rating2;
 
 console.log(`Количество просмотренных фильмов: ${numberOfFilms}`);
-console.log(`Последний просмотренный фильм: ${lastMovieTitle}`);
+console.log(`Один из последних просмотренных фильмов: ${lastMovieTitle}`);
 console.log(`Оценка: ${rating}`);
+console.log(`Один из последних просмотренных фильмов: ${lastMovieTitle2}`);
+console.log(`Оценка: ${rating2}`);
 console.log(personalMovieDB);
