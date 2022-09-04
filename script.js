8dffa9aa1cc3e17134b57a069af671ef75ed5eb1
Evgenij-Vasiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2022-09-04 19:36:16 +0300

    first lesson

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-console.log(numberOfFilms); 
-let personalMovieDB = {
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const lastMovieTitle = prompt("Какой фильм вы посмотрели последним?");
+const rating = prompt("Во сколько баллов(до 10) оцениваете фильм?");
+const personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {
+        title: lastMovieTitle,
+        score: rating,
+    },
     genres: [],
-    privat: false
+    privat: false,
 };
+
+console.log(`Количество просмотренных фильмов: ${numberOfFilms}`);
+console.log(`Последний просмотренный фильм: ${lastMovieTitle}`);
+console.log(`Оценка: ${rating}`);
 console.log(personalMovieDB);
