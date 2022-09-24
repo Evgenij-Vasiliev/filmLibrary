1f85dedf576a3376526a79f7bb049b52ab388de5
Evgenij-Vasiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2022-09-24 15:52:39 +0300

    modified script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 "use strict";
 
+<<<<<<< Updated upstream
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+=======
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = +prompt('Во сколько баллов(до 10) оцениваете фильм?', '');
+
+const a2= prompt('Один из последних просмотренных фильмов??', '');
+const b2= +prompt('Во сколько баллов(до 10) оцениваете фильм?', '');
+>>>>>>> Stashed changes
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,6 +18,7 @@ const personalMovieDB = {
     privat: false,
 };
 
+<<<<<<< Updated upstream
 for (let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
         b = +prompt("На сколько оцениваете его?", "");
@@ -22,4 +31,14 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+=======
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[a2] = b2;
+
+console.log(`Количество просмотренных фильмов: ${numberOfFilms}`);
+console.log(`Один из последних просмотренных фильмов: ${a}`);
+console.log(`Оценка: ${b}`);
+console.log(`Один из последних просмотренных фильмов: ${a2}`);
+console.log(`Оценка: ${b2}`);
+>>>>>>> Stashed changes
 console.log(personalMovieDB);
